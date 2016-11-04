5d67a99f042ac5f3105061cabe478670a1490d85
haydon19 <haydon.ruth19@gmail.com>
2016-11-04 16:09:12 -0400

    Add files via upload
    This is my current version of an orbit, to use this you must attach it to the Planet object, and change the variables for ship and planet to be the exact same as they are in your unity gameObject manager!

---
+++ src/Assets/Scripts/orbitPlanet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class orbitPlanet : MonoBehaviour {
+
+    public float gravitationalConstant = 10.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        var ship = GameObject.Find("pShip");
+        var planet = GameObject.Find("Planet");
+        Vector3 diff = planet.transform.position - ship.transform.position;
+        Vector3 direction = diff.normalized;
+        float gravitationalForce = (planet.GetComponent<Rigidbody2D>().mass * ship.GetComponent<Rigidbody2D>().mass * gravitationalConstant) / diff.sqrMagnitude;
+        ship.GetComponent<Rigidbody2D>().AddForce(direction * gravitationalForce);
+    }
+}
