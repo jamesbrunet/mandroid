643b11268ee60aa21060ce4a58efe474f97669af
James Brunet <jamesbrunet@live.ca>
2016-11-08 11:10:01 -0500

    Integrate collectibles from BallTutorial
    - Implement scoring system and win condition
    - Tile background 10x in every direction
    - Display score and game over screen

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
+// Our playercontroller is responsible for:
+// -Keeping track of its speed
+// -Picking up other game objects
+// -Keeping track of the score
+// The speed tracking, pickups, and score is from
+// the unity roll a ball tutorial
+
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+	public Text countText;
+	public Text winText;
 
     private Rigidbody rb;
+	private int score;
 
 	void Start () {
+		//This stops the device from falling asleep when only using the accelerometer
+		Screen.sleepTimeout = SleepTimeout.NeverSleep; 
+
         rb = GetComponent<Rigidbody>();
+		score = 0;
+		SetCountText ();
+		winText.text = "";
 	}
-	
+
 	void FixedUpdate () {
         
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			score += 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + score.ToString ();
+		if (score >= 12) {
+			winText.text = "You Win!";
+		}
 	}
 }
