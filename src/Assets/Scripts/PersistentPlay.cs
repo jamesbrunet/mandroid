0fec3b0b14db35ca4b725c1a17f10ba1ff097ac4
James Brunet <jamesbrunet@live.ca>
2016-12-06 02:00:23 -0500

    bugfix

--- src/Assets/Scripts/PersistentPlay.cs
+++ src/Assets/Scripts/PersistentPlay.cs
@@ -6,6 +6,10 @@ public class PersistentPlay : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad(this.gameObject);
+		if (FindObjectsOfType(GetType()).Length > 1)
+		{
+			Destroy(this.gameObject);
+		}
 	}
 	
 	// Update is called once per frame
