07fc1ad6268657d720842774c6793646fa2696f3
James Brunet <jamesbrunet@live.ca>
2017-03-18 13:36:42 -0400

    Delete everything
    EVERYTHING

--- src/Assets/Scripts/Main_Menu/ButtonHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine; using System.Collections;
-
-public class ButtonHandler : MonoBehaviour { 
-	public void LoadScene(string sceneName) { 
-		Application.LoadLevel(sceneName);
-	}
-}
